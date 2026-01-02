5157ceadd5f0e7778b1131b20f19fef0534c4f9a
Sofia <s.ahmed9833@gmail.com>
2026-01-02 21:30:58 +0000

    Setup env for Mapbox token and set biomejs as default formatter

--- src/main.ts
+++ src/main.ts
@@ -8,31 +8,33 @@ import mapboxgl, { type LngLatLike } from "mapbox-gl";
 const INITIAL_POSITION: LngLatLike = [-2.24455, 53.478];
 
 (async () => {
-  console.log("hello");
-  mapboxgl.accessToken = "<API_KEY_GOES_HERE>";
+	console.log("hello");
+	mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
-  const map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v12",
-    center: INITIAL_POSITION,
-    zoom: 9,
-  });
+	const map = new mapboxgl.Map({
+		container: "map",
+		style: "mapbox://styles/mapbox/streets-v12",
+		center: INITIAL_POSITION,
+		zoom: 9,
+	});
 
-  map.once("load", () => {
-    const overlay = new MapboxOverlay({
-      interleaved: true,
-      layers: [
-        new ScatterplotLayer({
-          id: "deckgl-circle",
-          data: [{ position: INITIAL_POSITION, color: [255, 0, 0], radius: 1000 }],
-          getPosition: (d) => d.position,
-          getFillColor: (d) => d.color,
-          getRadius: (d) => d.radius,
-          opacity: 0.3,
-        }),
-      ],
-    });
+	map.once("load", () => {
+		const overlay = new MapboxOverlay({
+			interleaved: true,
+			layers: [
+				new ScatterplotLayer({
+					id: "deckgl-circle",
+					data: [
+						{ position: INITIAL_POSITION, color: [255, 0, 0], radius: 1000 },
+					],
+					getPosition: (d) => d.position,
+					getFillColor: (d) => d.color,
+					getRadius: (d) => d.radius,
+					opacity: 0.3,
+				}),
+			],
+		});
 
-    map.addControl(overlay);
-  });
+		map.addControl(overlay);
+	});
 })();
